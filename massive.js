7a31eef511909ea7d448af6b40ee977567a32801
Sanjarbek Azizov <ya.s-azizov@yandex.ru>
2022-06-28 14:12:22 +0500

    first steps in new project

--- massive.js
+++ massive.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+const car =['opel', 'bmw', 'skoda', 'mazda'] // объявление массива
+const fib = [1, 1, 2, 3, 5, 8, 13]// add number in array
+car.push('peugeot')
+
+
+console.log(car)
