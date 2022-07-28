63221177ab685c43a5b2e150d3b46c639056a437
Sanjarbek Azizov <ya.s-azizov@yandex.ru>
2022-07-28 19:48:40 +0500

    i fimished  base course js

--- task1.js
+++ task1.js
@@ -158,4 +158,190 @@ alert("cancel")
 }
 else{
     alert("i don't know")
-}*/
+}
+############################
+############################
+############################
+let i=3;
+while(i){
+    alert(i--);
+}
+
+let i=0;
+while(++i<5) alert(i);
+let m=0;
+while(m++<5)alert(m)
+
+for (let i =0; i<5; ++i) alert(i);
+for (let m =0; m<5; m++) alert(m);
+
+for(let a=1; a<6; ++a){
+    alert(a*2)
+}
+/*####*/
+/*for(let i=2;i<=10;i++){
+    if(i%2==0){
+        alert(i)
+    }
+}
+let i=0;
+while(i<3){
+    
+    alert(`number ${i}!`);
+    i++
+}
+let num;
+do{
+    num=prompt("enter number more then 100",0)
+}
+while(num<=100&&num)
+
+let n = 10;
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  alert( i ); // простое число
+}
+let browser="Chrome";
+if(browser=="Edge"){
+alert("You've got the Edge")
+}
+else if(browser=="Chrome"||browser=="Firefox"||browser=="Safari"||browser=="Opera"){
+alert("Okay we support these browsers too")
+}
+else{
+    alert("We hope that this page looks ok!")
+}
+let number=prompt("enter any number",'0')
+switch(number){
+    case '0':
+        alert("Вы ввели число 0");
+        break;
+    case '1':
+        alert("Вы ввели число 1");
+        break;
+    case '2':
+    case '3':
+        alert("Вы ввели число 2, а может и 3");
+        break;    
+
+}
+let age=prompt("enter your age");
+function checkAge(age){
+return (age>18)? true: confirm('are parents allow');
+}
+function checkAges(age){
+return (age>18)|| confirm('are parents allow');
+}
+function min(a,b){
+if(a<b){
+    return alert(a)
+}
+else{
+    return alert(b)
+}
+}
+min(1,1)
+function mini(c,d){
+return c<d?alert(c):alert(d)
+}
+mini(1,-3) 
+function pow(p,n){
+return alert(Math.pow(p,n))
+}
+pow(5,3)
+function pow(x,n){
+    let result=x
+    for(let i=1;i<n;i++){
+        result*=x
+    }
+    return result
+} 
+let x = prompt("x?",'')
+let n = prompt("n?",'')
+if(n<1){
+    alert(`Степень ${n} не поддерживается, исрользуйте натуральное число`)
+}
+else{
+    alert(pow(x,n))
+}
+############################
+############################
+Функции-«колбэки»
+############################
+############################
+
+function ask(question,yes,no){
+    if(confirm(question)) yes()
+    else no()
+}
+function showOk(){
+    alert("you agree");
+}
+function  showCancel(){
+    alert("you cancel")
+}
+ask("are you agree", showOk,s)
+
+//with function experssion
+function ask(question, yes, no){
+ if(confirm(question)) yes()
+ else no();
+}
+ask(
+    "are you agree?",
+    function(){alert("you agree.");},
+    function(){alert("you cancel")}
+);
+
+// function declaration
+sayhi("John");
+function sayhi(name){
+    alert(`hi, ${name}`)
+}
+
+sayHi("John");
+let sayHi = function(name){
+    alert(`hi ${name}`);// магии больше нет
+}
+// правильный тип работы  ф-ции экспрессцион
+
+let age =prompt("how old are you?", 18)
+let welcome = (age<18)?
+function(){alert('Hi!');}:
+function(){alert('Good morning')}
+welcome(); 
+
+let sum=(a,b)=> a+b;
+alert(sum(5,6));
+///////// if you have one argument so arrow function looks like this
+let double=n=>n*2;
+alert(double(26));
+///////// if you have not argument so arrow function looks like this
+let sayHi=()=>("Hello");
+alert(sayHi());
+ let age =prompt("How old are you?",18);
+ let welcome=(age<18)?
+ ()=>alert("hi"):
+ ()=>alert("good morning")
+ welcome();
+ 
+let sum=(a,b)=>{
+let result=a+b;
+return result;
+}
+alert(sum(9,6));
+*/
+function ask (question,yes,no){
+    if(confirm(question))yes()
+    else no();
+}
+ask(
+    "are you agree?",
+    ()=>alert("you agreed"),
+    ()=>alert("you cancel")
+);
