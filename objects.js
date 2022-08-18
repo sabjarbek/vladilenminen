7653e753b42671db85ae6b88449be04aba754485
Sanjarbek Azizov <ya.s-azizov@yandex.ru>
2022-08-18 15:24:12 +0500

    i finiesd the function constraction

--- objects.js
+++ objects.js
@@ -556,4 +556,46 @@ function User(name) {
 let vasya = new User("Vasya");
 
 vasya.sayHi()
+
+ let obj = {};
+
+ function A() { return obj;}
+ function B() { return obj;}
+ alert( new A() == new B());
+ 
+function Calculator(){
+ this.read = function(){
+    this.a=+prompt("a?",0)
+    this.b=+prompt("b?",0)
+};
+
+this.sum = function(){
+    return this.a+this.b
+};
+
+this.mul= function(){
+    return this.a*this.b
+}
+}
+let calculator = new Calculator();
+ 
+calculator.read()
+
+alert( "sum=" + calculator.sum() );
+alert( "Mul="+ calculator.mul() )
+
+function Accumulator(startingValue){
+    this.value = startingValue;
+
+    this.read = function() {
+        this.value += +prompt('how much do need add?', 0)
+    }
+}
+
+let accumulator = new Accumulator(1);
+
+accumulator.read();
+accumulator.read();
+
+alert(accumulator.value);
 */
